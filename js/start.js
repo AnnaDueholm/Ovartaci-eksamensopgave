29d41fb5746dcd22e41adc4781aefbf2c80534e3
Thilde Bisgaard Jakobsen <eaa25tbja@students.eaaa.dk>
2026-05-27 15:03:25 +0200

    click event på startside

---
+++ js/start.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Lytter efter klik hvor som helst på siden
+document.addEventListener("click", () => {
+  // Sender brugeren videre til næste side
+  window.location.href = "input.html";
+});
