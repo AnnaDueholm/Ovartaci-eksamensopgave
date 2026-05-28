b30ee3b7efd6530613a1545eb13b721e18b99925
AnnaDueholm <eaa25alds@students.eaaa.dk>
2026-05-28 13:29:47 +0200

    keyboard
    Keyboard er gjort nogenlunde funktionelt, men der skal fortsat tilføjes mellemrum, punktum etc.

--- js/input.js
+++ js/input.js
@@ -42,3 +42,34 @@ backButton.addEventListener("click", () => {
   // Her sendes brugeren tilbage til input-siden
   window.location.href = "start.html";
 });
+
+/////////////////// TASTATUR /////////////////////////
+
+//Keyboard gøres funktionelt 
+//her tager jeg fat i mine html elementer 
+const textBox = document.querySelector("#story-input");
+const keys = document.querySelectorAll(".key");
+
+//Virtuel keyboard 
+
+//if sætning indsættes, så koden stopper hvis keys eller tekstfeltet ikke kan findes 
+//forEach bruges til at gennemløbe alle knapperne, og der sættes en addListener på, som lytter på knapper og kører koden når der trykkes, og teksten bliver skrevet i tekstboksen. 
+if (textBox && keys.length > 0) {
+  keys.forEach(key => {
+    key.addEventListener("click", () => {
+      textBox.value += key.innerText;
+      });
+      });
+    }
+
+//Fysisk keyboard 
+//lytter efter om en tast bliver trykket ned
+//if sætning indsættes, så koden stopper hvis keys eller tekstfeltet ikke kan findes 
+document.addEventListener("keydown", (event) => {     
+  if (!textBox) return;
+
+//sørger for at kun "almindelige taster" kan trykkes ned 
+  if (event.key.length === 1) {
+    textBox.value += event.key;
+    }
+    });
