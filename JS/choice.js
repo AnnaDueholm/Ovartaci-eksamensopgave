01721207c5e06a21a97f56607a44129dc91e6e7d
Thilde Bisgaard Jakobsen <eaa25tbja@students.eaaa.dk>
2026-06-01 10:31:52 +0200

    back-btn rettet, html rettet
    gjort html mere ens, så det blev nemmere at style tilbage knappen så den er ens på alle sider

--- JS/choice.js
+++ JS/choice.js
@@ -3,3 +3,13 @@
 document.addEventListener("click", () => {
   window.location.href = "input.html";
 });
+
+/////////////////// TILBAGE KNAP /////////////////////////
+// Her henter jeg tilbage-knappen fra html
+const backButton = document.querySelector("#choice-back-btn");
+
+// Her lytter jeg efter klik på tilbage-knappen
+backButton.addEventListener("click", () => {
+  // Her sendes brugeren tilbage til input-siden
+  window.location.href = "start.html";
+});
